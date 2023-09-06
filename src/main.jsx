4b5d647283fc242998aa0cbb80e3f21e56ef5d1f
Harlan George <hgeorge29@gmail.com>
2023-09-05 21:32:13 -0400

    updating main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ import Products from "./components/products/Products";
 import UpdateProduct from "./components/products/UpdateProducts";
 import DeleteProduct from "./components/products/DeleteProducts";
 //mport { loader as itemLoader } from "./components/products/ProductHelpers";
-import Users from "./components/user/manageUser";
+// import Users from "./components/user/manageUser";
 
 const router = createBrowserRouter([
   {
@@ -72,13 +72,13 @@ const router = createBrowserRouter([
         element: <UpdateProduct />,
       },
       {
-        path: "/products/:itemId/delete",
+        path: "./products/:itemId/delete",
         element: <DeleteProduct />,
       },
-      {
-        path: "/users/",
-        element: <Users />,
-      },
+      // {
+      //   path: "/users/",
+      //   element: <Users />,
+      // },
     ],
   },
 ]);
